2870c1ee4aafbc42028115a6999e8307279c1ef5
Yvan Dion <y.dion@geotraceability.com>
2017-01-17 15:39:11 -0500

    Adding Lobby facet

---
+++ MvcDemo/Models/LobbyistViewModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcDemo.Models
+{
+    
+    public class LobbyistViewModels
+    {
+        public int Id {get;set;}
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public bool IsRegistered { get; set; }
+	}
+}
